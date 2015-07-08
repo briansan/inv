b94230240fa49232a4bd25611bbcb0836c6160cf
bk <bkimstunnaboss@gmail.com>
2015-07-08 15:50:02 -0400

    worked on the method dump call of the api

--- py/api/view.py
+++ py/api/view.py
@@ -3,9 +3,20 @@ import json
 def request_login():
   return 'please log in first'
 
+def method_desc(name,desc,supports):
+  return {'name':name,'desc':desc,'supports':supports}
+
 def all_methods():
-  m = ['users','items','locations','assets','locations','logout']
-  return json.dumps(m)
+  m = [method_desc('add', 'inserts an entity', 'POST'),
+       method_desc('view', 'selects an entity', 'GET'),
+       method_desc('edit', 'updates an entity', 'POST'),
+       method_desc('remove', 'deletes an entity', 'GET')]
+  e = ['user','item','inventory','asset','location']
+  n = ['/{method}/{entity}/{description or id}',
+       '/{utility}']
+  u = ['login','logout','register','help']
+  d = {'methods':m,'entities':e,'navigation':n, 'utility':u}
+  return json.dumps(d,indent=4,separators=(',',': '))
 
 def login():
   return '<form action="" method="post"><p>Username: <input type=text name=uname></p></form>'
