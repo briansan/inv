d757c5050399fc17dfbee9fd0f8565293b3e8f9f
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-09 15:56:37 -0400

    made more modular with functions. ldap+inv

--- py/api/auth.py
+++ py/api/auth.py
@@ -5,24 +5,85 @@
           the Villanova LDAP server
 """
 
-import ldap
-from getpass import getpass
+def auth_ldap(uid,passwd):
+  """
+   using ldap to authenticate the user
+  """
+  import ldap
 
-uid = raw_input('Username: ')
-passwd = getpass('Password: ')
+  conn = ldap.initialize('ldaps://ldaps.villanova.edu')
+  results = conn.search_s('o=villanova.edu',ldap.SCOPE_SUBTREE,'uid='+uid)
+  if len(results) != 1:
+    return 'Invalid Username'
+  else:
+    info = results[0]
+    dn = info[0]
+    try:
+      chk = conn.bind_s(dn,passwd)
+    except:
+      return 'Wrong Password'
+  return info
 
-conn = ldap.initialize('ldaps://ldaps.villanova.edu')
-results = conn.search_s('o=villanova.edu',ldap.SCOPE_SUBTREE,'uid='+uid)
-if len(results) != 1:
-  print 'Invalid Username'
-  exit()
-else:
-  info = results[0]
-  dn = info[0]
-  try:
-    chk = conn.bind_s(dn,passwd)
-  except:
-    print 'Wrong Password'
-    exit()
+def auth_inv(uname,passwd):
+  """
+   inv authentication 
+  """
+  from model import User
 
-print 'Welcome ' + info[1]['cn'][0] + '!'
+  # try ldap
+  y = auth_ldap(uname,passwd)
+  # success
+  if type(y) is tuple:
+    # search for user in db
+    u = User.query.filter_by(uname=uname).first()
+    if not u: # user does not exist...
+      from methods import DefaultPermissions
+      # get the name
+      fname = y[1]['givenname'][0]
+      lname = y[1]['sn'][0]
+      # get the group type
+      ou = y[1]['ou'] # ou = organizational unit
+      grp = User.Groups.NoGroup
+      if 'Students' in ou:
+        grp = User.Groups.Student
+      elif 'Employees' in ou:
+        grp = User.Groups.Employee
+      # set permissions
+      perm = DefaultPermissions[grp]
+      # create the user
+      u = User(uname,fname,lname,grp,perm)
+      # add user to db
+      from model import db
+      db.session.add(u)
+      db.session.commit()
+    return u
+      
+  # failure
+  elif type(y) is str: 
+    return y
+
+  # other?
+  else:
+    raise Exception('The impossible has happened')
+
+def auth_io(inv=False):
+  """
+   an interactive authentication
+  """
+  from getpass import getpass
+  uid = raw_input('Username: ')
+  passwd = getpass('Password: ')
+  if inv:
+    print dict(auth_inv(uid,passwd))
+  else:
+    print auth_ldap(uid,passwd)
+  
+if __name__=="__main__":
+  import sys
+  if len(sys.argv) == 2 and sys.argv[1]=='inv':
+    from app import create_app
+    app = create_app('conf/debug.cfg')
+    with app.app_context():
+      auth_io(True)
+  else:
+    auth_io()
