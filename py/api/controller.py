ae92a45e2a76b2bdaebafe1a7f975dbe0a8f2ce6
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-09 15:57:10 -0400

    made more simpler with ldap auth

--- py/api/controller.py
+++ py/api/controller.py
@@ -5,21 +5,9 @@
           the methods described by inv/api/methods.py
 """
 
-from model import *
+import auth
 
-def user_exists(uname):
-  return len(User.query.filter_by(uname=uname).all())==1
 
-def login(uname):
+def login(uname,pw):
   # ZZ: alot more involved in authenticating here...
-  return user_exists(uname)
-
-def register(uname):
-  # ZZ: should do alot more checking here
-  if not user_exists(uname):
-    u = User(uname)
-    db.session.add(u)
-    db.session.commit()
-    return u.id
-  else:
-    return False
+  return auth.auth_inv(uname,pw)
