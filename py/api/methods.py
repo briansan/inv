9a7760ce81042021eb9ef7a8f67d2fcce5b26abe
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-13 17:08:54 -0400

    implemented location and item api methods

--- py/api/methods.py
+++ py/api/methods.py
@@ -37,6 +37,161 @@ ItemCatEdit  = 1 << 24
 ItemManView  = 1 << 25
 ItemManEdit  = 1 << 26
 
+# definition of the methods
+def save():
+  db.session.commit()
+
+def list_obj2dict(x):
+  """
+   converts a list of model objects to dictionaries
+  """
+  return [dict(y) for y in x]
+  
+"""
+ Location methods
+"""
+
+def create_building(x):
+  """
+   @param x str the name of the building
+   @return y LocationBuilding the object that was placed into the db or None for failure
+  """
+  y = None
+  exists = read_building(x) 
+  if not exists:
+    y = LocationBuilding(x)
+    db.session.add(y)
+    save()
+  return y
+
+def read_building(x):
+  """
+   @param x str the name of the building
+   @return y LocationBuilding or None 
+  """
+  return LocationBuilding.query.filter_by(name=x).first()
+
+def read_building_all():
+  """
+   @return list of dict representation of LocationBuildings
+  """
+  return list_obj2dict(LocationBuilding.query.filter_by().all())
+
+def create_location(x):
+  """
+  @param x dict key/value pairs of the location to add
+  """
+  # get the values
+  building = read_building(x['building'])
+  room = x['room']
+  # create a building entry if it doesn't exist
+  if not building:
+    building = create_building(x['building'])
+  # create and add the location
+  y = Location(building,room)
+  db.session.add(y)
+  save()
+  return y
+
+def read_location_by_building(x):
+  x = Location.query.filter(Location.building.has(name=x)).all()
+  return list_obj2dict(x)
+
+def read_location_all():
+  x = Location.query.filter_by().all()
+  return list_obj2dict(x)
+
+"""
+ Item methods
+"""
+
+def create_category(x):
+  """
+   @param x str the name of the building
+   @return y LocationBuilding the object that was placed into the db or None for failure
+  """
+  y = None
+  exists = read_category(x) 
+  if not exists:
+    y = ItemCategory(x)
+    db.session.add(y)
+    save()
+  return y
+
+def read_category(x):
+  """
+   @param x str the name of the category
+   @return y ItemCategory or None 
+  """
+  return ItemCategory.query.filter_by(name=x).first()
+
+def read_category_all():
+  """
+   @return list of dict representation of all item categories
+  """
+  return list_obj2dict(ItemCategory.query.filter_by().all())
+
+def create_manufacturer(x):
+  """
+   @param x str the name of the building
+   @return y LocationBuilding the object that was placed into the db or None for failure
+  """
+  y = None
+  exists = read_manufacturer(x) 
+  if not exists:
+    y = ItemManufacturer(x)
+    db.session.add(y)
+    save()
+  return y
+
+def read_manufacturer(x):
+  """
+   @param x str the name of the manufacturer
+   @return y ItemManufacturer or None 
+  """
+  return ItemManufacturer.query.filter_by(name=x).first()
+
+def read_category_all():
+  """
+   @return list of dict representation of all item manufacturers
+  """
+  return list_obj2dict(ItemManufacturer.query.filter_by().all())
+
+def create_item(x):
+  """
+   @param x dict key/value pairs of the item to add
+  """
+  # get the values
+  category = read_category(x['category'])
+  manufacturer = read_manufacturer(x['manufacturer'])
+  model = x['model']
+  # create a building entry if it doesn't exist
+  if not category:
+    category = create_category(x['category'])
+  if not manufacturer:
+    manufacturer = create_manufacturer(x['manufacturer'])
+  # create and add the item
+  y = Item(category,manufacturer,model)
+  db.session.add(y)
+  save()
+  return y
+
+def read_item_by_category(x):
+  x = Item.query.filter(Item.category.has(name=x)).all()
+  return list_obj2dict(x)
+
+def read_item_by_manufacturer(x):
+  x = Item.query.filter(Item.manufacturer.has(name=x)).all()
+  return list_obj2dict(x)
+
+def read_item_by_model(x):
+  x = Item.query.filter_by(model=x).all()
+  return list_obj2dict(x)
+
+def read_item_all():
+  x = Item.query.filter_by().all()
+  return list_obj2dict(x)
+
 # dictionary of information about the methods
 def method_desc(name,desc,usage,supports,more=None):
   return {'name':name,'desc':desc,'supports':supports,'usage':usage,'more':more}
