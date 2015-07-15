e38fe8ca4b104cbc4f70541ddd2ec741e933f496
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-15 17:00:47 -0400

    implemented asset+user methods as well as update+delete

--- py/api/methods.py
+++ py/api/methods.py
@@ -6,6 +6,7 @@
 """
 
 from model import *
+from datetime import datetime
 
 # definition of methods as bits
 LogInOut = 0
@@ -48,6 +49,45 @@ def list_obj2dict(x):
   return [dict(y) for y in x]
   
 """
+ User methods
+"""
+
+def read_user(id):
+  """
+   @param id int the id of the user
+  """
+  return User.query.filter_by(id=id).first()
+
+def read_user_all():
+  return list_obj2dict(User.query.filter_by().all())
+
+def update_user_self(x):
+  u = User.query.filter_by(id=id).first()
+  if not u: return False
+  u.fname = x.get('fname')
+  u.lname = x.get('lname')
+  save()
+  return u
+
+def update_user(id,x):
+  u = User.query.filter_by(id=id).first()
+  if not u: return False
+  u.fname = x.get('fname')
+  u.lname = x.get('lname')
+  u.grp = x.get('grp')
+  u.perm = x.get('perm')
+  u.start = x.get('start')
+  save()
+  return u
+
+def delete_user(id):
+  u = User.query.filter_by(id=id).first()
+  if not u: return False
+  db.session.delete(u)
+  save()
+  return True
+
+"""
  Location methods
 """
 
@@ -76,30 +116,83 @@ def read_building_all():
    @return list of dict representation of LocationBuildings
   """
   return list_obj2dict(LocationBuilding.query.filter_by().all())
+  
+def update_building(id,x):
+  """
+   @param id int the id of the building
+   @param x str the new name of the building
+   @return building LocationBuilding or None
+  """
+  building = LocationBuilding.query.filter_by(id=id).first()
+  if not building: return None
+  building.name = x
+  save()
+  return building
+
+def delete_building(id):
+  """
+   @param id int the id of the building to delete
+   @return bool True for success, False for failure
+  """
+  building = LocationBuilding.query.filter_by(id=id).first()
+  if not building: return False
+  db.session.delete(building)
+  save()
+  return True
 
 def create_location(x):
   """
   @param x dict key/value pairs of the location to add
+  @return y Location the object that was placed into the db or None for failure
   """
-  # get the values
-  building = read_building(x['building'])
-  room = x['room']
-  # create a building entry if it doesn't exist
-  if not building:
-    building = create_building(x['building'])
-  # create and add the location
-  y = Location(building,room)
-  db.session.add(y)
-  save()
+  y = None
+  exists = Location.query.filter(Location.building.has(name=x['building']),Location.room.is_(x['room'])).first()
+  if not exists:
+    # get the values
+    building = read_building(x['building'])
+    room = x['room']
+    # create a building entry if it doesn't exist
+    if not building:
+      building = create_building(x['building'])
+    # create and add the location
+    y = Location(building,room)
+    db.session.add(y)
+    save()
   return y
-
-def read_location_by_building(x):
-  x = Location.query.filter(Location.building.has(name=x)).all()
-  return list_obj2dict(x)
 
 def read_location_all():
   x = Location.query.filter_by().all()
   return list_obj2dict(x)
+
+def update_location(id,x):
+  """
+   @param id int the id of the building
+   @param x dict key/value pairs of the updated values
+   @return loc Location or None
+  """
+  loc = Location.query.filter_by(id=id).first()
+  if not loc: return None
+  # building name
+  new_build = x.get('building')
+  if loc.building.name != new_build:
+    build = read_building(new_build)
+    build = create_building(new_build) if not build else build
+    loc.building = build
+  # room number
+  loc.room = x.get('room')
+  save()
+  return loc
+
+def delete_location(id):
+  """
+   @param id int the id of the location to delete
+   @return bool True for success, False for failure
+  """
+  loc = Location.query.filter_by(id=id).first()
+  if not loc: return False
+  db.session.delete(loc)
+  save()
+  return True
 
 """
  Item methods
@@ -108,7 +201,7 @@ def read_location_all():
 def create_category(x):
   """
    @param x str the name of the building
-   @return y LocationBuilding the object that was placed into the db or None for failure
+   @return y ItemCategory the object that was placed into the db or None for failure
   """
   y = None
   exists = read_category(x) 
@@ -131,6 +224,30 @@ def read_category_all():
   """
   return list_obj2dict(ItemCategory.query.filter_by().all())
 
+def update_category(id,x):
+  """
+   @param id int the id of the category
+   @param x str the new name of the category
+   @return cat Category or None
+  """
+  cat = Category.query.filter_by(id=id).first()
+  if not cat: return None
+  # category name
+  cat.name = x.get('name')
+  save()
+  return cat
+
+def delete_category(id):
+  """
+   @param id int the id of the category to delete
+   @return bool True for success, False for failure
+  """
+  cat = Category.query.filter_by(id=id).first()
+  if not cat: return False
+  db.session.delete(cat)
+  save()
+  return True
+  
 def create_manufacturer(x):
   """
    @param x str the name of the building
@@ -151,46 +268,190 @@ def read_manufacturer(x):
   """
   return ItemManufacturer.query.filter_by(name=x).first()
 
-def read_category_all():
+def read_manufacturer_all():
   """
    @return list of dict representation of all item manufacturers
   """
   return list_obj2dict(ItemManufacturer.query.filter_by().all())
 
+def update_manufacturer(id,x):
+  """
+   @param id int the id of the manufacturer
+   @param x str the new name of the manufacturer
+   @return man ItemManufacturer or None
+  """
+  man = ItemManufacturer.query.filter_by(id=id).first()
+  if not man: return None
+  # manufacturer name
+  man.name = x.get('name')
+  save()
+  return man
+
+def delete_manufacturer(id):
+  """
+   @param id int the id of the manufacturer to delete
+   @return bool True for success, False for failure
+  """
+  man = ItemManufacturer.query.filter_by(id=id).first()
+  if not man: return False
+  db.session.delete(man)
+  save()
+  return True
+
+def item_exists(x):
+  category = Item.category.has(name=x['category'])
+  manufacturer = Item.category.has(name=x['manufacturer'])
+  model = Item.category.is_(x['model'])
+  return Item.query.filter(category,manufacturer,model).first()
+
 def create_item(x):
   """
    @param x dict key/value pairs of the item to add
   """
-  # get the values
-  category = read_category(x['category'])
-  manufacturer = read_manufacturer(x['manufacturer'])
-  model = x['model']
-  # create a building entry if it doesn't exist
-  if not category:
-    category = create_category(x['category'])
-  if not manufacturer:
-    manufacturer = create_manufacturer(x['manufacturer'])
-  # create and add the item
-  y = Item(category,manufacturer,model)
-  db.session.add(y)
-  save()
+  y = None
+  if not item_exists(x):
+    # get the values
+    category = read_category(x['category'])
+    manufacturer = read_manufacturer(x['manufacturer'])
+    model = x['model']
+    # create a building entry if it doesn't exist
+    if not category:
+      category = create_category(x['category'])
+    if not manufacturer:
+      manufacturer = create_manufacturer(x['manufacturer'])
+    # create and add the item
+    y = Item(category,manufacturer,model)
+    db.session.add(y)
+    save()
   return y
-
-def read_item_by_category(x):
-  x = Item.query.filter(Item.category.has(name=x)).all()
-  return list_obj2dict(x)
-
-def read_item_by_manufacturer(x):
-  x = Item.query.filter(Item.manufacturer.has(name=x)).all()
-  return list_obj2dict(x)
-
-def read_item_by_model(x):
-  x = Item.query.filter_by(model=x).all()
-  return list_obj2dict(x)
 
 def read_item_all():
   x = Item.query.filter_by().all()
   return list_obj2dict(x)
+
+def update_item(id,x):
+  """
+   @param id int the id of the item
+   @param x dict key/value pairs of the updated values
+   @return i Item or None
+  """
+  i = Item.query.filter_by(id=id).first()
+  if not i: return None
+  # item category
+  new_cat = x.get('category')
+  if i.category.name != new_cat:
+    cat = read_category(new_cat)
+    cat = create_category(new_cat) if not cat else cat
+    i.category = cat
+  # item manufacturer
+  new_man = x.get('manufacturer')
+  if i.manufacturer.name != new_man:
+    man = read_manufacturer(new_man)
+    man = create_manufacturer(new_man) if not man else man
+    i.manufacturer = man
+  # item model
+  i.model = x.get('model')
+  save()
+  return i
+
+def delete_item(id):
+  """
+   @param id int the id of the item to delete
+   @return bool True for success, False for failure
+  """
+  i = Item.query.filter_by(id=id).first()
+  if not i: return False
+  db.session.delete(i)
+  save()
+  return True
+
+"""
+  asset methods
+"""
+
+def asset_exists(x):
+  return Asset.query.filter(Asset.tag.has(ece=x['tag_ece'])).first()
+
+def create_asset(x):
+  """
+   @param x dict key/value pairs of the asset to add
+  """
+  y = None
+  if not asset_exists(x):
+    # get the values
+    ece = x['tag_ece']
+    vu = x['tag_vu']
+    unit = x['tag_unit']
+    svc = x['tag_svc']
+    serial = x['tag_serial']
+    status = x['status']
+    item_id = x['item'] #
+    purchased_since1970 = x['purchased']
+    purchased = datetime.fromtimestamp(purchased_since1970) if purchased_since1970 else None
+    img = x['img']
+    owner_id = x['owner'] #
+    holder_id = x['holder'] #
+    price = x['price']
+    receipt = x['receipt']
+    ip = x['ip']
+    comments = x['comments']
+    home_id = x['home'] #
+    current_id = x['current'] #
+    # convert id's into objects
+    tag = AssetInfo(ece,vu,unit,svc,serial)
+    item = Item.query.filter_by(id=item_id).first()
+    owner = User.query.filter_by(id=owner_id).first()
+    holder = User.query.filter_by(id=holder_id).first()
+    home = Location.query.filter_by(id=home_id).first()
+    current = Location.query.filter_by(id=current_id).first()
+    # create the asset
+    y = Asset(tag,status,item,purchased,img,owner,holder,price,receipt,ip,comments,home,current)
+    # insert into db
+    db.session.add(y)
+    save()
+  return y
+
+def read_asset_all():
+  return list_obj2dict(Asset.query.filter_by().all())
+
+def update_asset(id,x):
+  """
+   @param id int the id of the asset to update
+   @param x dict key/value pairs of the updated values
+   @return a Asset or None
+  """
+  a = Asset.query.filter_by(id=id).first()
+  if not a: return None
+  a.tag.ece = x['tag_ece']
+  a.tag.vu = x['tag_vu']
+  a.tag.unit = x['tag_unit']
+  a.tag.svc = x['tag_svc']
+  a.tag.serial = x['tag_serial']
+  a.status = x['status']
+  a.item = Item.query.filter_by(id=x['item']).first()
+  a.purchased = date.fromtimestamp(x['purchased']) if x['purchased'] else None
+  a.img = x['img']
+  a.owner = User.query.filter_by(id=x['owner'])
+  a.holder = User.query.filter_by(id=x['holder'])
+  a.price = x['price']
+  a.receipt = x['receipt']
+  a.ip = x['ip']
+  a.comments = x['comments']
+  a.home = User.query.filter_by(id=x['home'])
+  a.current = User.query.filter_by(id=x['current'])
+  save()
+  return a
+
+def delete_asset(id):
+  """
+   @param id int the id of the asset to delete
+  """
+  a = Asset.query.filter_by(id=id).first()
+  if not a: return False
+  db.session.delete(a.tag)
+  db.session.delete(a)
+  save()
+  return True
 
 # dictionary of information about the methods
 def method_desc(name,desc,usage,supports,more=None):
