16a482049b45619f668fa8715ac620a794e4f29b
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-16 17:04:09 -0400

    got rid of angular code, just some convenience variables

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,2 @@
-(function() {
-  var app = angular.module('inv',[])
-
-  var server = 'http://vecr.ece.villanova.edu:8000';
-
-  app.controller('APIController', function($scope,$http,$sce) {
-    this.data = null; 
-    this.login = null;
-    self=this;
-
-    $http.get(server).
-    success(function(data,status,headers,config) {
-      self.data = data.msg;
-    }).     
-    error(function(data,status,headers,config) {
-    });     
-
-    $http.get(server+'/login').
-    success(function(data,status,headers,config) {
-      self.login = $sce.trustAsHtml(data)
-    }).
-    error(function(data,status,headers,config) {
-    });
-  });     
-})(); 
+var api = new APIController();
+var print = function(data) {console.log(data);}
