39adbfdb414ee00e7e183eb4efdd2bc11b8518f9
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-16 17:04:26 -0400

    implemented all the core api calls

--- www/js/api.js
+++ www/js/api.js
@@ -4,6 +4,34 @@
 // @brief  a script that defines an api handler using jquery
 //
 
+//
+// convenience methods for making the model objects
+// (probably should be placed elsewhere...)
+//
+var build_location = function(building,room) {
+  return {building:building,room:room};
+}
+
+var build_item = function(category,manufacturer,model) {
+  return {category:category,manufacturer:manufacturer,model:model};
+}
+
+var build_asset = function(ece,vu,unit,svc,serial,
+                           status,item,purchased,img,
+                           owner,holder,price,receipt,
+                           ip,comments,home,current) {
+  return {
+    tag_ece:ece, tag_vu:vu, tag_unit:unit, tag_svc:svc, tag_serial:serial,
+    status:status, item:item, purchased:purchased, img:img, owner:owner,
+    holder: holder, price:price, receipt:receipt, ip:ip, comments:comments,
+    home: home, current:current
+  }
+}
+
+var build_inv = function(who,what,when,where) {
+  return {who:who,what:what,when:when,where:where}
+}
+
 var APIController = function(server) {
   // initialization
   if (server == null) server = 'http://vecr.ece.villanova.edu:8000';
@@ -106,9 +134,8 @@ var APIController = function(server) {
   }
 
   // location
-  this.add_location = function(building, room, callback) {
-    data = {building:building,room:room};
-    this.inv_ajax('/add/location','POST',data,callback);
+  this.add_location = function(location, callback) {
+    this.inv_ajax('/add/location','POST',location,callback);
   }
 
   this.get_location = function(id,callback) {
@@ -119,12 +146,76 @@ var APIController = function(server) {
     this.inv_ajax('/view/location','GET',null,callback);
   }
 
-  this.set_location = function(id,building,room,callback) {
+  this.set_location = function(id,location,callback) {
     data = {building:building,room:room};
-    this.inv_ajax('/edit/location/'+id,'POST',data,callback);
+    this.inv_ajax('/edit/location/'+id,'POST',location,callback);
   }
 
   this.delete_location = function(id,callback) {
     this.inv_ajax('/delete/location/'+id,'GET',null,callback);
   }
+
+  // item
+  this.add_item = function(item,callback) {
+    this.inv_ajax('/add/item','POST',item,callback);
+  };
+
+  this.get_item = function(id,callback) {
+    this.inv_ajax('/view/item/'+id,'GET',null,callback);
+  };
+
+  this.get_item_all = function(callback) {
+    this.inv_ajax('/view/item','GET',null,callback);
+  };
+
+  this.set_item = function(id,item,callback) {
+    this.inv_ajax('/edit/item/'+id,'POST',item,callback);
+  };
+
+  this.delete_item = function(id,callback) {
+    this.inv_ajax('/rm/item/'+id,'GET',null,callback);
+  };
+
+  // asset
+  this.add_asset = function(asset,callback) {
+    this.inv_ajax('/add/asset','POST',asset,callback);
+  };
+
+  this.get_asset = function(id,callback) {
+    this.inv_ajax('/view/asset/'+id,'GET',null,callback);
+  };
+
+  this.get_asset_all = function(callback) {
+    this.inv_ajax('/view/asset','GET',null,callback);
+  };
+
+  this.set_asset = function(id,asset,callback) {
+    this.inv_ajax('/edit/asset','POST',asset,callback);
+  };
+
+  this.delete_asset = function(id,callback) {
+    this.inv_ajax('/rm/asset','GET',null,callback);
+  };
+
+  // inv
+  this.add_inv = function(inv,callback) {
+    this.inv_ajax('/add/inv','POST',inv,callback);
+  };
+
+  this.get_inv = function(id,callback) {
+    this.inv_ajax('/view/inv/'+id,'GET',inv,callback);
+  };
+
+  this.get_inv_all = function(callback) {
+    this.inv_ajax('/view/inv','GET',null,callback);
+  };
+
+  this.set_inv = function(id,inv,callback) {
+    this.inv_ajax('/edit/inv/'+id,'POST',inv,callback);
+  };
+
+  this.delete_inv = function(id,callback) {
+    this.inv_ajax('/rm/inv/'+id,'GET',callback);
+  };
+
 };
