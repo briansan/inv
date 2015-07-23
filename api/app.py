002b57bffc3aa75e6a82b7a73098ffe3bc6eaca4
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-23 17:08:11 -0400

    completely changed around the routing

--- api/app.py
+++ api/app.py
@@ -1,90 +1,271 @@
-from flask import Flask, session, request, Blueprint
-import model, view, methods, controller
-from crossdomain import crossdomain
+"""
+ @file   inv/api/app.py
+ @author Brian Kim
+ @brief  top level server script
+"""
 
-api = Blueprint('admin',__name__)
-origin='http://153.104.47.49:8100'
+from flask import Flask, session, request, Blueprint, jsonify, g
+
+import controller, model, view
+from auth import auth_ldap
+from util import *
+
+api = Blueprint('api',__name__)
+origin = '*'
 
 @api.route('/')
-@crossdomain(origin=origin)
+@crossdomain()
+@requires_auth
 def root():
   return controller.root()
 
-@api.route('/login',methods=['GET','POST'])
-@crossdomain(origin=origin)
-def login():
-  return controller.login()
-      
-@api.route('/logout',methods=['GET','POST'])
-@crossdomain(origin=origin)
-def logout():
-  return controller.logout()
+@api.route('/item', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def item():
+  if is_create():
+    y = controller.add_item
+  elif is_read():
+    y = controller.view_item
+  else:
+    y = view.invalid_method
+  return y()
 
-@api.route('/add/<entity>', methods=['POST'])
-@crossdomain(origin=origin)
-def add(entity):
-  return controller.add(entity)
+@api.route('/item/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def item_id(id):
+  if is_read():
+    y = controller.view_item
+  elif is_update():
+    y = controller.edit_item
+  elif is_delete():
+    y = controller.rm_item
+  else:
+    y = view.invalid_method
+  return y(id)
 
-@api.route('/count/<entity>/<by>')
-@crossdomain(origin=origin)
-def count_all(entity):
-  return controller.count(entity,by)
+@api.route('/item/category', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def category():
+  if is_create():
+    y = controller.add_category
+  elif is_read():
+    y = controller.view_category
+  else:
+    y = view.invalid_method
+  return y()
 
-@api.route('/count/<entity>/<by>/<id>')
-@crossdomain(origin=origin)
-def count(entity,by,id):
-  return controller.count(entity,by,id)
+@api.route('/item/category/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def category_id(id):
+  if is_read():
+    y = controller.view_category
+  elif is_update():
+    y = controller.edit_category
+  elif is_delete():
+    y = controller.rm_category
+  else:
+    y = view.invalid_method
+  return y(id)
 
-@api.route('/view/<entity>')
-@crossdomain(origin=origin)
-def vw_all(entity):
-  return vw(entity,0)
+@api.route('/item/manufacturer', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def manufacturer():
+  if is_create():
+    y = controller.add_manufacturer
+  elif is_read():
+    y = controller.view_manufacturer
+  else:
+    y = view.invalid_method
+  return y()
 
-@api.route('/view/<entity>/<id>')
-@crossdomain(origin=origin)
-def vw(entity,id):
-  return controller.vw(entity,int(id))
+@api.route('/item/manufacturer/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def manufacturer_id(id):
+  if is_read():
+    y = controller.view_manufacturer
+  elif is_update():
+    y = controller.edit_manufacturer
+  elif is_delete():
+    y = controller.rm_manufacturer
+  else:
+    y = view.invalid_method
+  return y(id)
 
-@api.route('/view/user')
-@crossdomain(origin=origin)
-def vw_self():
-  return controller.view_self()
+@api.route('/location', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def location():
+  if is_create():
+    y = controller.add_location
+  elif is_read():
+    y = controller.view_location
+  else:
+    y = view.invalid_method
+  return y()
 
-@api.route('/view/user/<id>')
-@crossdomain(origin=origin)
-def vw_user(id):
-  return controller.view_user(id)
+@api.route('/location/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def location_id(id):
+  if is_read():
+    y = controller.view_location
+  elif is_update():
+    y = controller.edit_location
+  elif is_delete():
+    y = controller.rm_location
+  else:
+    y = view.invalid_method
+  return y(id)
 
-@api.route('/view/user/group')
-@crossdomain(origin=origin)
-def vw_user_groups():
-  return view.success(model.User.Groups.info)
+@api.route('/location/building', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def building():
+  if is_create():
+    y = controller.add_building
+  elif is_read():
+    y = controller.view_building
+  else:
+    y = view.invalid_method
+  return y()
 
-@api.route('/view/asset/status')
-@crossdomain(origin=origin)
-def vw_asset_statuses():
-  return view.success(model.Asset.Status.info)
+@api.route('/location/building/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def building_id(id):
+  if is_read():
+    y = controller.view_building(id)
+  elif is_update():
+    y = controller.edit_building(id)
+  elif is_delete():
+    y = controller.rm_building(id)
+  else:
+    y = view.invalid_method()
+  return y
 
-@api.route('/edit/<entity>/<id>', methods=['POST'])
-@crossdomain(origin=origin)
-def edit(entity,id):
-  return controller.edit(entity,id)
+@api.route('/asset', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def asset():
+  if is_create():
+    y = controller.add_asset()
+  elif is_read():
+    y = controller.view_asset()
+  else:
+    y = view.invalid_method()
+  return y
 
-@api.route('/rm/<entity>/<id>')
-@crossdomain(origin=origin)
-def rm(entity,id):
-  return controller.rm(entity,id)
+@api.route('/asset/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def asset_id(id):
+  if is_read():
+    y = controller.view_asset(id)
+  elif is_update():
+    y = controller.edit_asset(id)
+  elif is_delete():
+    y = controller.rm_asset(id)
+  else:
+    y = view.invalid_method()
+  return y
 
-def create_app(fname):
+@api.route('/asset/<id>/img', methods=['GET','POST','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def asset_img():
+  if is_create():
+    y = 'create asset img'
+  elif is_read():
+    y = 'read asset img'
+  elif is_update():
+    y = 'update asset img'
+  elif is_delete():
+    y = 'delete asset img'
+  else:
+    y = view.invalid_method()
+  return y
+
+@api.route('/asset/<id>/img/thumbnail', methods=['GET'])
+@crossdomain()
+@requires_auth
+def asset_img_thumbnail():
+  if is_read():
+    y = 'read asset img thumbnail'
+  else:
+    y = view.invalid_method()
+  return y
+  
+@api.route('/asset/<id>/receipt', methods=['GET','POST','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def asset_receipt():
+  if is_create():
+    y = 'create asset img'
+  elif is_read():
+    y = 'read asset img'
+  elif is_update():
+    y = 'update asset img'
+  elif is_delete():
+    y = 'delete asset img'
+  else:
+    y = view.invalid_method()
+  return y
+
+@api.route('/asset/<id>/img/thumbnail', methods=['GET'])
+@crossdomain()
+@requires_auth
+def asset_receipt_thumbnail():
+  if is_read():
+    y = 'read asset img thumbnail'
+  else:
+    y = view.invalid_method()
+  return y()
+  
+@api.route('/inv', methods=['GET','POST'])
+@crossdomain()
+@requires_auth
+def inv():
+  if is_create():
+    y = controller.add_inv()
+  elif is_read():
+    y = controller.view_inv()
+  else:
+    y = view.invalid_method()
+  return y
+
+@api.route('/inv/<id>', methods=['GET','PUT','DELETE'])
+@crossdomain()
+@requires_auth
+def inv_id(id):
+  if is_read():
+    y = controller.view_inv(id)
+  elif is_update():
+    y = controller.edit_inv(id)
+  elif is_delete():
+    y = controller.rm_inv(id)
+  else:
+    y = view.invalid_method()
+  return y
+
+def create_app(conf):
+  """
+   use this method to create and instance of the app for serving
+  """
   # initialize and configure the app
   app = Flask(__name__)
-  app.config.from_pyfile(fname)
+  app.config.from_pyfile(conf)
   # connect the model to the app
   model.db.init_app(app)
   with app.app_context():
     model.db.create_all()
   # register blueprints
-  app.register_blueprint(api)
+  app.register_blueprint(api, url_prefix='/api/v1')
   return app
 
 if __name__=="__main__":
