0223dd8ec29a9621e3c7114c053068d60d2796fa
Brian Kim <bkim11@vecr.ece.villanova.edu>
2015-07-23 17:11:32 -0400

    crossdomain file replaced by util file for more general convenience method implementations

--- api/util.py
+++ api/util.py
@@ -1,9 +1,54 @@
+"""
+ @file   inv/api/util.py
+ @author Brian Kim
+ @brief  a script that defines utility methods for the server
+"""
+
+from auth import auth_inv
 from datetime import timedelta
-from flask import make_response, request, current_app
-from functools import update_wrapper
+from functools import update_wrapper, wraps
+from flask import current_app, make_response, request, Response
 
+"""
+ convenience methods to determine the http method used
+"""
 
-def crossdomain(origin=None, methods=None, headers=None,
+def is_create():
+  return request.method == 'POST'
+
+def is_read():
+  return request.method == 'GET'
+
+def is_update():
+  return request.method == 'PUT'
+  
+def is_delete():
+  return request.method == 'DELETE'
+
+"""
+ authentication decorator
+"""
+
+def authenticate():
+  """sends a 401 resposne that enables basic auth"""
+  msg = 'Could not verify your access level for that URL.\n'
+  msg += 'You have to login with proper credentials'
+  header = {'WWW-Authenticate': 'Basic realm="Login Required"'}
+  return Response( msg, 401, header )
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    auth = request.authorization
+    if not auth or not auth_inv(auth.username, auth.password):
+      return authenticate()
+    return f(*args, **kwargs)
+  return decorated
+
+"""
+ cross-origin resource sharing decorator
+"""
+def crossdomain(origin='*', methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
                 automatic_options=True):
   if methods is not None:
@@ -11,6 +56,7 @@ def crossdomain(origin=None, methods=None, headers=None,
   if headers is not None and not isinstance(headers, basestring):
     headers = ', '.join(x.upper() for x in headers)
   if not isinstance(origin, basestring):
+    print origin
     origin = ', '.join(origin)
   if isinstance(max_age, timedelta):
     max_age = max_age.total_seconds()
@@ -37,7 +83,7 @@ def crossdomain(origin=None, methods=None, headers=None,
       h['Access-Control-Allow-Methods'] = get_methods()
       h['Access-Control-Max-Age'] = str(max_age)
       h['Access-Control-Allow-Credentials'] = 'true'
-      h['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type, Accept, Origin'
+      h['Access-Control-Allow-Headers'] = 'X-Requested-With'
       if headers is not None:
         h['Access-Control-Allow-Headers'] = headers
       return resp
