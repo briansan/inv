a8e0460590f7acd651357197e7fc96f34b07f58c
bk <bkimstunnaboss@gmail.com>
2015-07-30 13:52:55 -0400

    more human friendly

--- setup.sh
+++ setup.sh
@@ -7,22 +7,29 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # install necessary 3rd party pkg
-apt-get update
-apt-get install apache2 libapache2-mod-wsgi mysql-server python-dev python-pip
+echo "inv: updating Ubuntu"
+apt-get update &> /dev/null
+echo "inv: installing pkgs"
+apt-get install apache2 libapache2-mod-wsgi mysql-server python-dev python-pip -y &> /dev/null
+echo "inv: installing python pkgs"
 pip install -r requirements.txt
 
 # setup mysql
+echo "inv: initializing database"
 mysql -u root -p -e "create database inv"; 
 
 # setup dir
+echo "inv: setting up inv directory"
 cp -r server /var/www/inv
 
 # setup ssl
-a2enmod ssl
-mkdir /etc/apache2/ssl
+echo "inv: setting up ssl"
+a2enmod ssl &> /dev/null
+mkdir /etc/apache2/ssl 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
 
 # setup apache
+echo "inv: setting up apache"
 cp conf/inv.conf /etc/apache2/sites-available
-a2ensite inv
+a2ensite inv &> /dev/null
 service apache2 reload
