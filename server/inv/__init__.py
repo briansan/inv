676a4a809a73680c30b50b9ff4e1a22561aa373b
bk <bkimstunnaboss@gmail.com>
2015-08-03 12:23:16 -0400

    implemented create_app so now you can from inv import create_app

--- server/inv/__init__.py
+++ server/inv/__init__.py
@@ -1,2 +1,27 @@
-from app import create_app
-app = create_app('conf/deploy.cfg')
+"""
+ @file   inv/__init__.py
+ @author Brian Kim
+ @brief  the app initialization script for inv
+"""
+
+from flask import Flask
+from flask_sslify import SSLify
+from api import api
+from model import db
+
+def create_app(conf):
+  # init
+  app = Flask(__name__)
+  app.config.from_pyfile(conf)
+  # connect model to app
+  db.init_app(app)
+  with app.app_context():
+    db.create_all()
+  # secure it
+  SSLify(app)
+  # register blueprints
+  app.register_blueprint(api,url_prefix='/api/v1')
+  return app
+
+if __name__=="__main__":
+  create_app('conf/debug.cfg').run(host='0.0.0.0')
