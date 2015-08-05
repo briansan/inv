b73217dd8efd7ed1e1169ecd53a9214c55aad7f5
bk <bkimstunnaboss@gmail.com>
2015-08-05 13:41:13 -0400

    got rid of permissions (moved to auth), cleaned up user stuff

--- server/inv/methods.py
+++ server/inv/methods.py
@@ -8,36 +8,6 @@
 from model import *
 from datetime import datetime
 
-# definition of methods as bits
-LogInOut = 0
-UserReadSelf  = 1 << 0
-UserReadWorld = 1 << 1
-UserUpdateSelf  = 1 << 2
-UserUpdateWorld = 1 << 3
-UserDelete = 1 << 4
-ItemCreate = 1 << 5
-ItemRead   = 1 << 6
-ItemUpdate = 1 << 7
-ItemDelete = 1 << 8
-LocationCreate = 1 << 9
-LocationRead   = 1 << 10
-LocationUpdate = 1 << 11
-LocationDelete = 1 << 12
-AssetCreate = 1 << 13
-AssetRead   = 1 << 14
-AssetUpdate = 1 << 15
-AssetDelete = 1 << 16
-InvCreate   = 1 << 17
-InvRead     = 1 << 18
-InvUpdate   = 1 << 19
-InvDelete   = 1 << 20
-LocBuildView = 1 << 21
-LocBuildEdit = 1 << 22
-ItemCatView  = 1 << 23
-ItemCatEdit  = 1 << 24
-ItemManView  = 1 << 25
-ItemManEdit  = 1 << 26
-
 # definition of the methods
 def save():
   db.session.commit()
@@ -60,25 +30,6 @@ def read_user(id):
 
 def read_user_all():
   return list_obj2dict(User.query.filter_by().all())
-
-def update_user_self(x):
-  u = User.query.filter_by(id=id).first()
-  if not u: return False
-  u.fname = x.get('fname')
-  u.lname = x.get('lname')
-  save()
-  return u
-
-def update_user(id,x):
-  u = User.query.filter_by(id=id).first()
-  if not u: return False
-  u.fname = x.get('fname')
-  u.lname = x.get('lname')
-  u.grp = x.get('grp')
-  u.perm = x.get('perm')
-  u.start = x.get('start')
-  save()
-  return u
 
 def delete_user(id):
   u = User.query.filter_by(id=id).first()
@@ -396,25 +347,24 @@ def create_asset(x):
     unit = x['tag_unit']
     svc = x['tag_svc']
     serial = x['serial']
+
     status = x['status']
     item_id = x['item'] #
-    purchased_since1970 = x['purchased']
-    purchased = datetime.fromtimestamp(purchased_since1970) if purchased_since1970 else None
-    owner_id = x['owner'] #
-    holder_id = x['holder'] #
     price = x['price']
     ip = x['ip']
     comments = x['comments']
+
+    purchased_since1970 = x['purchased']
+    purchased = datetime.fromtimestamp(purchased_since1970) if purchased_since1970 else None
+    owner_id = x['owner'] #
     home_id = x['home'] #
-    current_id = x['current'] #
+
     # convert id's into objects
     item = Item.query.filter_by(id=item_id).first()
     owner = User.query.filter_by(id=owner_id).first()
-    holder = User.query.filter_by(id=holder_id).first()
     home = Location.query.filter_by(id=home_id).first()
-    current = Location.query.filter_by(id=current_id).first()
     # create the asset
-    y = Asset(ece,status,item,purchased,owner,holder,home,current,comments,price,ip,vu,unit,svc,serial)
+    y = Asset(ece,status,item,comments,price,ip,purchased,owner,home,vu,unit,svc,serial)
     # insert into db
     db.session.add(y)
     save()
@@ -434,21 +384,23 @@ def update_asset(id,x):
   """
   a = read_asset(id)
   if not a: return None
-  a.tag_ece = x['tag_ece']
-  a.tag_vu = x['tag_vu']
-  a.tag_unit = x['tag_unit']
-  a.tag_svc = x['tag_svc']
-  a.serial = x['serial']
-  a.status = x['status']
-  a.item = Item.query.filter_by(id=x['item']).first()
-  a.purchased = date.fromtimestamp(x['purchased']) if x['purchased'] else None
-  a.owner = User.query.filter_by(id=x['owner'])
-  a.holder = User.query.filter_by(id=x['holder'])
-  a.price = x['price']
-  a.ip = x['ip']
-  a.comments = x['comments']
-  a.home = User.query.filter_by(id=x['home'])
-  a.current = User.query.filter_by(id=x['current'])
+  if x.get('tag_ece'): a.tag_ece = x.get('tag_ece')
+  if x.get('tag_vu'): a.tag_vu = x.get('tag_vu')
+  if x.get('tag_unit'): a.tag_unit = x.get('tag_unit')
+  if x.get('tag_svc'): a.tag_svc = x.get('tag_svc')
+  if x.get('serial'): a.serial = x.get('serial')
+  if x.get('status'): a.status = x.get('status')
+  if x.get('item'): a.item = read_item(x.get('item'))
+  if x.get('price'): a.price = x['price']
+  if x.get('ip'): a.ip = x['ip']
+  if x.get('comments'): a.comments = x['comments']
+  if x.get('purchased'): 
+    a.purchased = date.fromtimestamp(x['purchased']) 
+  if x.get('owner'):
+    a.owner = User.query.filter_by(id=x['owner'])
+  if x.get('home'):
+    a.home = Location.query.filter_by(id=x['home'])
+  
   save()
   return a
 
@@ -509,142 +461,4 @@ def delete_inv(id):
   if not inv: return False
   db.session.delete(inv)
   save()
-
-# dictionary of information about the methods
-def method_desc(name,desc,usage,supports,more=None):
-  return {'name':name,'desc':desc,'supports':supports,'usage':usage,'more':more}
-
-info = {
-LogInOut: method_desc('Login',
-                      'authenticate into inv',
-                      'POST',
-                      '/login',
-                      {'uname':'username','passwd':'password'}),
-UserReadSelf: method_desc('View Self',
-                          'read user information about yourself',
-                          'GET',
-                          '/view/user'),
-UserReadWorld: method_desc('View World',
-                            'read user information about other people',
-                            'GET',
-                            '/view/user?{query}',
-                            User.info()),
-UserUpdateSelf: method_desc('Edit Self',
-                             'update user information about yourself',
-                             'POST',
-                             '/edit/user',
-                             {'fname':'first name','lname':'last name'}),
-UserUpdateWorld: method_desc('Edit World',
-                             'update user information about other people',
-                             'POST',
-                             '/edit/user/{id}',
-                             User.info()),
-UserDelete: method_desc('Remove World',
-                        'delete a user from inv',
-                        'DELETE',
-                        '/remove/user/{id}'),
-ItemCreate: method_desc('Add Item',
-                        'create an item',
-                        'POST',
-                        '/add/item',
-                        Item.info()),
-ItemRead: method_desc('View Item',
-                      'read information about an item',
-                      'GET',
-                      '/view/item?{query}',
-                      Item.info()),
-ItemUpdate: method_desc('Edit Item',
-                        'update information of an item',
-                        'POST',
-                        '/edit/item/{id}',
-                        Item.info()),
-ItemDelete: method_desc('Remove Item',
-                        'delete an item from inv',
-                        'DELETE',
-                        '/remove/item/{id}'),
-LocationCreate: method_desc('Add Location',
-                            'create a location',
-                            'POST',
-                            '/add/location',
-                            Location.info()),
-LocationRead: method_desc('View Location',
-                          'read information about a location',
-                          'GET',
-                          '/view/location?{query}',
-                          Location.info()),
-LocationUpdate: method_desc('Edit Location',
-                            'update information about a location',
-                            'POST',
-                            '/edit/location/{id}',
-                            Location.info()),
-LocationDelete: method_desc('Remove Location',
-                            'delete a location from inv',
-                            'DELETE',
-                            '/remove/location/{id}'),
-AssetCreate: method_desc('Add Asset',
-                         'creates an asset',
-                         'POST',
-                         '/add/asset',
-                         Asset.info()),
-AssetRead: method_desc('View Asset',
-                       'read information about an asset',
-                       'GET',
-                       '/view/asset?{query}',
-                       Asset.info()),
-AssetUpdate: method_desc('Edit Asset',
-                         'update information about an asset',
-                         'POST',
-                         '/edit/asset/{id}',
-                         Asset.info()),
-AssetDelete: method_desc('Remove Asset',
-                         'delete an asset from inv',
-                         'DELETE',
-                         '/remove/asset/{id}'),
-InvCreate: method_desc('Add Inventory',
-                       'create an inventory record',
-                       'POST',
-                       '/add/inv',
-                       Inventory.info()),
-InvRead: method_desc('View Inventory',
-                     'read the inventory log',
-                     'GET',
-                     '/view/inv?{query}',
-                     Inventory.info()),
-InvUpdate: method_desc('Edit Inventory',
-                       'update an inventory record',
-                       'POST',
-                       '/edit/inv/{id}',
-                       Inventory.info()),
-InvDelete: method_desc('Remove Inventory',
-                       'deletes an inventory record from inv',
-                       'DELETE',
-                       '/remove/inv/{id}'),
-LocBuildView: method_desc('View Location Building',
-                          'read the list of buildings',
-                          'GET',  
-                          '/view/building?{query}'),
-LocBuildEdit: method_desc('Edit Location Building',
-                          'add, modify, or remove a building',
-                          'POST', 
-                          '/add/building OR /edit/building/{id} OR /rm/building/{id}',
-                          LocationBuilding.info()),
-ItemCatView: method_desc('View Item Category',
-                          'read the list of item categories',
-                          'GET',  
-                          '/view/category?{query}'),
-ItemCatEdit: method_desc('Edit Item Category',
-                          'add, modify, or remove an item category',
-                          'POST', 
-                          '/add/category OR /edit/category/{id} OR /rm/category/{id}',
-                          ItemCategory.info()),
-ItemManView: method_desc('View Item Manufacturer',
-                          'read the list of item manufacturers',
-                          'GET',  
-                          '/view/manufacturer?{query}'),
-ItemManEdit: method_desc('Edit Item Manufacturer',
-                         'add, modify, or remove an item manufacturer',
-                         'POST',
-                         '/add/manufacturer OR /edit/manufacturer/{id} OR /rm/manufacturer/{id}',
-                         ItemManufacturer.info()),
-}
 
